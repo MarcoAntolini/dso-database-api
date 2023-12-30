8198e5875e32b2ecf565763a923e72a1bf58474f
Marco Antolini <marco.antolini2001@gmail.com>
2023-12-30 19:18:22 +0100

    update cors settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: "*",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,HEAD,POST,PUT,DELETE,OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type, Authorization, Content-Length, Accept, Accept-Encoding, x-requested-with",
+					},
+				],
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
