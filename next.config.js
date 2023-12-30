11a2891757d825003e7e732342aac15b69b59ad7
Marco Antolini <marco.antolini2001@gmail.com>
2023-12-30 19:25:04 +0100

    undo changes

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	async headers() {
-		return [
-			{
-				source: "*",
-				headers: [
-					{
-						key: "Access-Control-Allow-Origin",
-						value: "*",
-					},
-					{
-						key: "Access-Control-Allow-Methods",
-						value: "GET,HEAD,POST,PUT,DELETE,OPTIONS",
-					},
-					{
-						key: "Access-Control-Allow-Headers",
-						value: "Content-Type, Authorization, Content-Length, Accept, Accept-Encoding, x-requested-with",
-					},
-				],
-			},
-		];
-	},
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
